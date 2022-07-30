43eeb72d42eec6c5a1a9ca76127d26d18ea141aa
Vítor Tozzi <vitor.tozzi@xpi.com.br>
2022-07-30 18:23:08 -0300

    Commit inicial e setup do Hilt

---
+++ app/src/main/java/br/com/pokebase/PokeBaseApplication.kt
@@ -0,0 +1,7 @@
+package br.com.pokebase
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PokeBaseApplication: Application()
---
+++ app/src/main/java/br/com/pokebase/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,17 @@
+package br.com.pokebase.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class DashboardViewModel @Inject constructor(
+) : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}

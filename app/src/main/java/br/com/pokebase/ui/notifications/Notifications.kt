14dcfeb909bbf9dd5530f70167401f7e730ed43b
vitortozzi <vitor.tozzi@gmail.com>
2026-05-14 11:17:40 -0300

    chore: add local config files to gitignore

---
+++ app/src/main/java/br/com/pokebase/ui/notifications/Notifications.kt
@@ -0,0 +1,19 @@
+package br.com.pokebase.ui.notifications
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+fun NotificationsScreen(viewModel: NotificationsViewModel = hiltViewModel()) {
+    val text by viewModel.text.collectAsState()
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text(text = text)
+    }
+}

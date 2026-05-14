81df4a17ce6cd708a3dbce23982a33fe157178cb
vitortozzi <vitor.tozzi@gmail.com>
2026-05-14 17:29:21 -0300

    chore: add a TODO for details fetch improvement

--- app/src/main/java/br/com/pokebase/data/repository/PokemonCatalogRepositoryImpl.kt
+++ app/src/main/java/br/com/pokebase/data/repository/PokemonCatalogRepositoryImpl.kt
@@ -11,6 +11,7 @@ class PokemonCatalogRepositoryImpl @Inject constructor(
 ) : PokemonCatalogRepository {
     override suspend fun getPokemons(limit: Int, offset: Int): List<PokemonDetail> {
         val response = apiService.getPokemonList(limit, offset)
+        //TODO: This is by far not performatic. I will be migrating this to a better solution on the next days
         return response.results.map {
             apiService.getPokemonDetail(it.name).toDomain()
         }
